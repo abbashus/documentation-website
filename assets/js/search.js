948bc38ea441c319760ebdf28d848dc9b36c3509
Miki <mehranb@amazon.com>
2021-10-21 00:38:45 -0700

    Add search result highlighting and keyboard control Use versioned docs and prepare for website search
    Signed-off-by: Miki <mehranb@amazon.com>

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,60 +1,124 @@
 (() => {
-    console.log('Search...');
     const elInput = document.getElementById('search-input');
     const elResults = document.getElementById('search-results');
-    if (!elInput || !elResults) return;
+    const elOverlay = document.querySelector('.search-overlay');
+    if (!elInput || !elResults || !elOverlay) return;
+
+    const canSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+
+    const docsVersion = elInput.getAttribute('data-docs-version');
+
+    let _showingResults = false,
+        debounceTimer,
+        lastQuery;
+
+    const abortControllers = [];
 
     elInput.addEventListener('input', e => {
         debounceInput();
     });
 
-    let debounceTimer;
+    elInput.addEventListener('keydown', e => {
+        switch (e.key) {
+            case 'Esc':
+            case 'Escape':
+                hideResults(true);
+                elInput.value = '';
+                break;
+
+            case 'ArrowUp':
+                e.preventDefault();
+                highlightNextResult(false);
+                break;
+
+            case 'ArrowDown':
+                e.preventDefault();
+                highlightNextResult();
+                break;
+
+            case 'Enter':
+                e.preventDefault();
+                navToHighlightedResult();
+                break;
+        }
+    });
+
+    elInput.addEventListener('focus', e => {
+        if (!_showingResults && elResults.textContent) showResults();
+    });
+
     const debounceInput = () => {
         clearTimeout(debounceTimer);
-        setTimeout(doSearch, 500);
+        debounceTimer = setTimeout(doSearch, 300);
     };
 
-    let lastQuery;
+    function abortPreviousCalls() {
+        while (abortControllers.length) abortControllers.pop()?.abort?.();
+    }
+
     const doSearch = async () => {
         const query = elInput.value.replace(/[^a-z0-9-_. ]+/ig, ' ');
-        if (query.length < 3) return hideResults();
+        if (query.length < 3) return hideResults(true);
         if (query === lastQuery) return;
 
         lastQuery = query;
-        const response = await fetch(`https://search-api.opensearch.org/search?q=${query}`);
-        const data = await response.json();
-        if (!Array.isArray(data?.results)) {
-            return hideResults();
-        }
-        const chunks = data.results.map(result => result
-            ? `
-            <div>
-                <a href="${result.url}">
-                    <cite>${result.ancestors?.map?.(text => `${text} › `).join('')}</cite>
-                    ${result.title}
-                </a>
-                <span>${result.content?.replace?.(/\n/g, '&hellip; ')}</span>
-            </div>
-            `
-            : ''
-        );
 
-        emptyResults();
-        elResults.appendChild(document.createRange().createContextualFragment(chunks.join('')));
-        showResults();
+        abortPreviousCalls();
+
+        try {
+            const controller = new AbortController();
+            abortControllers.unshift(abortControllers);
+            const response = await fetch(`https://search-api.opensearch.org/search?q=${query}&v=${docsVersion}`, {signal: controller.signal});
+            const data = await response.json();
+            if (!Array.isArray(data?.results) || data.results.length === 0) {
+                return showNoResults();
+            }
+            const chunks = data.results.map(result => result
+                ? `
+                <div class="custom-search-result">
+                    <a href="${result.url}">
+                        <cite>${result.type === 'DOCS' ? `OpenSearch ${result.version} › ` : ''}${result.ancestors?.join?.(' › ')}</cite>
+                        ${result.title}
+                    </a>
+                    <span>${result.content?.replace?.(/\n/g, '&hellip; ')}</span>
+                </div>
+                `
+                : ''
+            );
+
+            emptyResults();
+            elResults.appendChild(document.createRange().createContextualFragment(chunks.join('')));
+            showResults();
+        } catch (ex) {}
     }
 
-    const hideResults = () => {
-        console.log('hideResults');
+    const hideResults = destroy => {
+        _showingResults = false;
         document.documentElement.classList.remove('search-active');
         elResults.setAttribute('aria-expanded', 'false');
-        emptyResults();
+        document.body.removeEventListener('pointerdown', handlePointerDown, false);
+
+        if (destroy) {
+            emptyResults();
+            lastQuery = '';
+        }
     };
 
     const showResults = () => {
-        console.log('showResults');
+        elResults.scrollTo(0, 0);
+
+        if (_showingResults) return;
+
+        _showingResults = true;
         document.documentElement.classList.add('search-active');
         elResults.setAttribute('aria-expanded', 'true');
+        document.body.addEventListener('pointerdown', handlePointerDown, false);
+    };
+
+    const showNoResults = () => {
+        emptyResults();
+        elResults.appendChild(document.createRange().createContextualFragment('<span>No results found!</span>'));
+        showResults();
     };
 
     const emptyResults = () => {
@@ -64,5 +128,51 @@
 
     const _clean = text => {
         return text?.replace?.(/<.*>/g, '');
+    };
+
+    const handlePointerDown = e => {
+        if (e.target.matches('.search-input-wrap, .search-input-wrap *, .search-results, .search-results *')) return;
+
+        e.preventDefault();
+
+        elInput.blur();
+        hideResults();
+    };
+
+    const highlightNextResult = (down = true) => {
+        const highlighted = elResults.querySelector('.custom-search-result.highlighted');
+        let nextResult;
+        if (highlighted) {
+            highlighted.classList.remove('highlighted');
+            nextResult = highlighted[down ? 'nextElementSibling' : 'previousElementSibling']
+        } else {
+            nextResult = elResults.querySelector(`.custom-search-result:${down ? 'first' : 'last'}-child`);
+        }
+
+        if (nextResult) {
+            nextResult.classList.add('highlighted');
+            if (down) {
+                if (canSmoothScroll) {
+                    nextResult.scrollIntoView({behavior: "smooth", block: "end"});
+                } else {
+                    nextResult.scrollIntoView(false)
+                }
+            } else if (
+                nextResult.offsetTop < elResults.scrollTop ||
+                nextResult.offsetTop + nextResult.clientHeight > elResults.scrollTop + elResults.clientHeight
+            ) {
+                if (canSmoothScroll) {
+                    elResults.scrollTo({behavior: "smooth", top: nextResult.offsetTop, left: 0});
+                } else {
+                    elResults.scrollTo(0, nextResult.offsetTop);
+                }
+            }
+        } else {
+            elResults.scrollTo(0, 0);
+        }
+    };
+
+    const navToHighlightedResult = () => {
+        elResults.querySelector('.custom-search-result.highlighted a[href]')?.click?.();
     };
 })();
--- assets/js/version-selector.tpl
+++ assets/js/version-selector.tpl
@@ -4,6 +4,7 @@ permalink: /assets/js/version-selector.js
 (() => {
 {% assign current_array = site.data.versions.current | split: '!' %}
 {% assign all_versions = current_array | concat: site.data.versions.past %}
+const DOC_LATEST_VERSION = '{{ site.data.versions.current }}';
 const DOC_VERSIONS = {{ all_versions | jsonify }};
 {% include_relative _version-selector.js %}
 })();
